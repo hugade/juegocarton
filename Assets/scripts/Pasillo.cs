f0d45a027b43d2e790fdff3404f7f28e2c0ef436
hugade <hugade@alumnatflorida.es>
2024-09-19 11:51:32 +0200

    primera subida

---
+++ Assets/scripts/Pasillo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pasillo : MonoBehaviour
+{
+    public float speed;
+
+    private float position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Translate(new Vector3(0, 0, -1) * Time.deltaTime * speed, Space.World);
+      
+        
+    }
+}
